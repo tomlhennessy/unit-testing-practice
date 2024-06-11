767f1a11eccf64f55e30967da3de51b68829b002
Tom Hennessy <tomlhennessy@outlook.com>
2024-06-11 18:13:41 +0100

    passed tests

--- funcs.js
+++ funcs.js
@@ -4,6 +4,7 @@ function isFive(num) {
 
 function isOdd(number) {
   if (typeof number !== 'number') throw new Error('Input must be a number');
+  return number % 2 !== 0;
 }
 
 function myRange(min, max, step = 1) {
